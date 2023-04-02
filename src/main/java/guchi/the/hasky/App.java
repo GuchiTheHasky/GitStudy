f198e2a6750b48f991f353cfeeafc58252d967b0
GuchiTheHasky <guchithehasky@gmail.com>
2023-04-02 23:36:25 +0300

    first

---
+++ src/main/java/guchi/the/hasky/App.java
@@ -0,0 +1,13 @@
+package guchi.the.hasky;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "First init!" );
+    }
+}
